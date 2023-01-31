0bd10373aa8d6f288547edda959d5c19440c5f5d
Felipems999 <felipems97@hotmail.com>
2023-01-31 14:27:53 -0300

    Criando templates

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,20 @@
 from app import app
+from flask import render_template
+
 
 @app.route('/')
-@app.route('/index')
 def index():
-    return 'Hello World!!!'
+
+    user = {'username': 'Melissa'}
+    posts = [{
+        'author': 'Felipe',
+        'text': 'Aprendendo Python Flask, um microframework para criar webapp',
+        'date': '22/01/2023'
+    },
+        {
+            'author': 'Melissa',
+            'text': 'Visitando hospital na aula de hoje!',
+            'date': '20/01/2023'
+    }]
+
+    return render_template('index.html', title='home page', user=user, posts=posts)
